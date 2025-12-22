2e0163a1be52118a40cdfe8814dc7e82454a7508
Sunny Hirai <thesunny@gmail.com>
2025-12-22 13:06:40 -0800

    Configure

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -3,6 +3,7 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import turboPlugin from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
 import onlyWarn from "eslint-plugin-only-warn";
+import unusedImports from "eslint-plugin-unused-imports";
 
 /**
  * A shared ESLint configuration for the repository.
@@ -16,9 +17,20 @@ export const config = [
   {
     plugins: {
       turbo: turboPlugin,
+      "unused-imports": unusedImports,
     },
     rules: {
       "turbo/no-undeclared-env-vars": "warn",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
     },
   },
   {
